89649f188a561425ed9baa4a69dc6e8b843cd9a5
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-11-22 11:50:10 -0800

    First Update

---
+++ Python_Calismalarim/Temel/Degiskenler.py
@@ -0,0 +1,10 @@
+sayi=10 #integer
+deger="10"#string
+tc="1010101010"
+floatSayi=10.0
+
+print(float(tc) + 10)
+print(type(floatSayi))
+print(type(sayi))
+print(type(tc))
+
---
+++ Python_Calismalarim/Temel/Strings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 20 08:00:11 2022
+
+@author: ibrah
+"""
+#mesaj = "Merhaba dünya"
+#print(mesaj[2:5])
+nes="deneme"
+print(nes[1:4])
+# Dizinin 4 numaralıindeksi dahil değil
+atama=nes[:3]
+print(atama)
+print(len(atama))
+Boyut=atama[len(atama)-1:len(atama)]
+print(Boyut)
+
+print(atama.upper())
+print(atama.lower())
+
