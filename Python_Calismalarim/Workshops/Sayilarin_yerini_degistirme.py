489bb9b483e826b81d11457f028167cad15d4b67
ibrahimkebapci <60468692+ibrahimkebapci@users.noreply.github.com>
2022-12-08 11:49:01 -0800

    Update

---
+++ Python_Calismalarim/Workshops/Sayilarin_yerini_degistirme.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+x = 10
+y = 20
+
+print("x = " + str(x))
+print("x = " + str(y))
+
+#pythona özel metod
+#x,y= y,x
+
+#Klasik metod
+temp = x
+x = y
+y = temp
+
+print ("yeni x = " + str(x))
+print ("yeni y = " + str(y)) 
